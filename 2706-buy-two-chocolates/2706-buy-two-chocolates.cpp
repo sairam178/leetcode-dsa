ec993c5e05dfef8e8f894f08984ffe288e24ad75
sairam178 <sairamlaggoni178@gmail.com>
2026-08-07 20:16:41 +0530

    Time: 0 ms (100%), Space: 52.3 MB (32.16%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        int n = prices.size();
+        sort(prices.begin(),prices.end());
+        int sum = prices[0]+prices[1];
+        if(sum <=money){
+            return money-sum;
+        }
+        return money;
+    }
+};
