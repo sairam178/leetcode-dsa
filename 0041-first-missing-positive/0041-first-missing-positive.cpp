d4ed55243f6423d354d4b9a4c91b7f8ded575a76
Laggoni Sairam goud <sairamlaggoni178@gmail.com>
2026-08-27 18:44:06 +0530

    Time: 123 ms (5.01%), Space: 84.3 MB (5.15%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        vector<int>vec;
+        for(auto x:nums){
+            if(x>0){
+                vec.push_back(x);
+            }
+        }
+
+        int n = vec.size();
+        int count=1;
+        set<int> arr;
+        for(auto x:vec){
+            arr.insert(x);
+        }
+        vec.clear();
+        for(auto x:arr){
+            vec.push_back(x);
+        }
+        sort(vec.begin(),vec.end());
+
+        for(int i=0;i<n;i++){
+            if(vec[i]==count){
+                 count++;
+            }else{
+                return count;
+            }
+            
+        }
+        return count++;
+    }
+};
